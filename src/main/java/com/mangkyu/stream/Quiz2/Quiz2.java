e8e878f97ea7a091a404bfde25d7dc7bd78b0c4f
kihwankim <clack2933@gmail.com>
2022-06-26 15:09:52 +0900

    feat: 2번 문제 풀이 완료

--- src/main/java/com/mangkyu/stream/Quiz2/Quiz2.java
+++ src/main/java/com/mangkyu/stream/Quiz2/Quiz2.java
@@ -1,20 +1,24 @@
 package com.mangkyu.stream.Quiz2;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Quiz2 {
 
     private static List<String> WORDS = Arrays.asList("TONY", "a", "hULK", "B", "america", "X", "nebula", "Korea");
 
     public Map<String, Integer> quiz1() {
-        return new HashMap<>();
+        return WORDS.stream()
+                .map(str -> Character.toString(str.charAt(0)))
+                .collect(Collectors.toMap(prefix -> prefix, prefix -> 1, Integer::sum));
     }
 
     public String quiz2() {
-        return null;
+        return WORDS.stream()
+                .filter(str -> str.length() > 1)
+                .map(str -> str.substring(0, 1).toUpperCase())
+                .collect(Collectors.joining(" "));
     }
-
 }
