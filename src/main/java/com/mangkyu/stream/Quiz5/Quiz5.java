78bd4b6fb60db9a1c9289d464bceea3227760333
kihwankim <clack2933@gmail.com>
2022-07-19 21:43:23 +0900

    feat: quiz 5문제 해결

--- src/main/java/com/mangkyu/stream/Quiz5/Quiz5.java
+++ src/main/java/com/mangkyu/stream/Quiz5/Quiz5.java
@@ -1,26 +1,44 @@
 package com.mangkyu.stream.Quiz5;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Quiz5 {
 
     private static final String[] STRING_ARR = {"aaa", "bb", "c", "dddd"};
 
     public int quiz1() {
-        return 0;
+        return Arrays.stream(STRING_ARR)
+                .mapToInt(String::length)
+                .sum();
     }
 
     public int quiz2() {
-        return 0;
+        return Arrays.stream(STRING_ARR)
+                .mapToInt(String::length)
+                .max()
+                .orElseThrow(IllegalAccessError::new);
     }
 
     public List<Integer> quiz3() {
-        return Collections.emptyList();
+        return new Random().ints(1, 46)
+                .distinct()
+                .limit(6)
+                .sorted()
+                .boxed()
+                .collect(Collectors.toList());
     }
 
     public List<Integer[]> quiz4() {
-        return Collections.emptyList();
+        return IntStream.rangeClosed(1, 6)
+                .boxed()
+                .flatMap(value -> IntStream.rangeClosed(1, 6).boxed().map(left -> new Integer[]{value, left}))
+                .filter(arr -> arr[0] + arr[1] == 6)
+                .collect(Collectors.toList());
+
     }
 
 }
